4f76aeedc9b9acae0e93ae9f69bf74487b74cb69
xandervdh <xander-v.d.h@hotmail.com>
2020-09-16 10:56:40 +0200

    made the fetches to get the weather for the week of the chosen city

--- script/weather.js
+++ script/weather.js
@@ -1,5 +1,34 @@
 (()=>{
+    const key = "e17453c206fbb634ac54f3ae7f51483f";
+    let cityInput;
 
+    document.getElementById("run").addEventListener("click", function (){
+        checkInput();
+        let url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid="
+        fetchData(url, getCoords);
+    })
 
+    function fetchData(url, func) {
+        fetch(url + key)
+            .then(response => response.json())
+            .then (data => func(data))
+    }
+
+    function getCoords(data) {
+        console.log(data);
+        let lon = data.city.coord.lon;
+        let lat = data.city.coord.lat;
+        let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=";
+        fetchData(url, getWeek);
+    }
+
+    function getWeek(data) {
+        console.log(data);
+
+    }
+
+    function checkInput() {
+        cityInput = document.getElementById("cityinput").value;
+    }
 
 })();
